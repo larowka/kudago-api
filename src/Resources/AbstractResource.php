18b19621791a33ac36ca446e1fdde956dc84c2a4
larowka <larowka@icloud.com>
2021-11-27 00:20:34 +0300

    Implement Arrayable interface

--- src/Resources/AbstractResource.php
+++ src/Resources/AbstractResource.php
@@ -2,10 +2,11 @@
 
 namespace Larowka\KudaGo\Resources;
 
+use Illuminate\Contracts\Support\Arrayable;
 use Larowka\KudaGo\CollectionFactory;
 use Larowka\KudaGo\Resources\Traits\HasAttributes;
 
-abstract class AbstractResource implements IResource
+abstract class AbstractResource implements IResource, Arrayable
 {
     use HasAttributes;
 
@@ -14,6 +15,28 @@ abstract class AbstractResource implements IResource
         $instance = new static();
         $instance->map($data);
         return $instance;
+    }
+
+    public function toArray(): array
+    {
+        $result = [];
+        foreach (static::$attributes as $key => $item) {
+            if ($value = isset($this->$key) ? $this->$key : false) {
+                if ($item === true) {
+                    $result[$key] = $value;
+                } elseif (is_array($item)) {
+                    $collection = [];
+                    foreach ($value as $resource) {
+                        $collection = $resource->toArray();
+                    }
+                    $result[$key] = $collection;
+                } else {
+                    $result[$key] = $value->toArray();
+                }
+            }
+        }
+
+        return $result;
     }
 
     private function map(array $data): void
